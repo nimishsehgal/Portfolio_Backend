c66b3d0dbca84ea6bcca6b55d007159f6a3092be
nimishsehgal <nimish.sehgal33@gmail.com>
2025-04-15 20:07:42 +0530

    Setup Done

---
+++ main.py
@@ -0,0 +1,18 @@
+# backend/main.py
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/ping")
+def ping():
+    return {"message": "Pong from FastAPI!"}
